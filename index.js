ad282ff3d3aa25230792e82354a38e0f63cdb7bd
Thomas Rains <thomasrains01@gmail.com>
2024-09-26 12:18:05 -0700

    submits intro to arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+// Write your solution here
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newcatsarray = [...cats, name]
+    return newcatsarray
+}
+
+function prependCat(name) {
+    const newcatsarray = [name, ...cats]
+    return newcatsarray
+}
+
+function removeLastCat() {
+    const copyOfCatsSlice = cats.slice(0, -1)
+    return copyOfCatsSlice
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1)
+    return copyOfCats
+}
